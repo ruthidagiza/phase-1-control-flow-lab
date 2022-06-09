6d2e4982df05e9cd7ddc5c0d6ac4ec302f68638d
IDAGIZA <ruthidagiza@gmail.com>
2022-06-09 21:24:19 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(valueDistance) {
+  if (valueDistance <= 400) {
+    return "This one is on me!";
+  } else if (valueDistance >= 2000 && valueDistance <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destination) {
   // Write your code here!
+  return destination === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+switch(tip){
+  case "generous":
+    return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+      default:
+      return "Bye.";
+}
+
 }
